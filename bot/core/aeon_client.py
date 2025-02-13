2953f21c0f522d9b7b772f99a28cf686ab4baced
Johnmclane5 <134460067+Johnmclane5@users.noreply.github.com>
2025-02-13 20:32:18 +0530

    Update aeon_client.py

--- bot/core/aeon_client.py
+++ bot/core/aeon_client.py
@@ -27,7 +27,6 @@ class TgClient:
             Config.TELEGRAM_HASH,
             bot_token=Config.BOT_TOKEN,
             parse_mode=enums.ParseMode.HTML,
-            max_concurrent_transmissions=10
         )
         await cls.bot.start()
         cls.NAME = cls.bot.me.username
@@ -45,7 +44,6 @@ class TgClient:
                     session_string=Config.USER_SESSION_STRING,
                     parse_mode=enums.ParseMode.HTML,
                     no_updates=True,
-                    max_concurrent_transmissions=10
                 )
                 await cls.user.start()
                 cls.IS_PREMIUM_USER = cls.user.me.is_premium
