01253fc01851ef870db9fdcc7fd13099c07307ed
Johnmclane5 <134460067+Johnmclane5@users.noreply.github.com>
2025-02-18 02:50:01 +0530

    Update poster_gen.py

--- bot/helper/aeon_utils/poster_gen.py
+++ bot/helper/aeon_utils/poster_gen.py
@@ -84,11 +84,11 @@ async def get_movie_poster(movie_name, release_year):
 
                         # Use the backdrop_path or poster_path
                             poster_path = None
-                            if 'backdrops' in movie_images and movie_images['backdrops']:
-                                poster_path = movie_images['backdrops'][0]['file_path']
+                            #if 'backdrops' in movie_images and movie_images['backdrops']:
+                                #poster_path = movie_images['backdrops'][0]['file_path']
                                                         
-                            elif 'backdrop_path' in result and result['backdrop_path']:
-                                poster_path = result['backdrop_path']
+                            if 'poster_path' in result and result['poster_path']:
+                                poster_path = result['poster_path']
                             poster_url = f"https://image.tmdb.org/t/p/original{poster_path}"
                             return poster_url
     except Exception as e:
